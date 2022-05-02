7c7c0d10585d9ae36ae1de63bd234829653875d1
AdvikaGit <104129106+AdvikaGit@users.noreply.github.com>
2022-05-02 17:42:00 +0530

    Add files via upload

---
+++ Angular/Day4/Task2/employee-list6.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../models/Employee';
+@Component({
+  selector: 'app-employee-list6',
+  templateUrl: './employee-list6.component.html',
+  styleUrls: ['./employee-list6.component.css']
+})
+export class EmployeeList6Component 
+{
+
+  EmpArray:Employee[]=[
+    {Empno:1,EmpName:"Smith",EmoJob:"Developer",Salary:20000,Dept:"Sales"},
+    {Empno:2,EmpName:"Steves",EmoJob:"manager",Salary:42234,Dept:"Admin"},
+    {Empno:3,EmpName:"Andrew",EmoJob:"HR",Salary:89000,Dept:"Admin"},
+    {Empno:4,EmpName:"Smith",EmoJob:"Developer",Salary:34000,Dept:"Sales"},
+    {Empno:5,EmpName:"Steves",EmoJob:"manager",Salary:23000,Dept:"Admin"},
+    {Empno:6,EmpName:"Andrew",EmoJob:"HR",Salary:67000,Dept:"Admin"},
+    {Empno:7,EmpName:"Smith",EmoJob:"Developer",Salary:34000,Dept:"Sales"},
+   
+  ];
+
+
+
+
+}
