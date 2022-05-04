b127dcd344456813a21c3b64b8470e5cbcd8b0f7
AdvikaGit <104129106+AdvikaGit@users.noreply.github.com>
2022-05-04 20:33:15 +0530

    Add files via upload

---
+++ Angular/Day5/Task1/input-list1.component.ts
@@ -0,0 +1,77 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input-list1',
+  templateUrl: './input-list1.component.html',
+  styleUrls: ['./input-list1.component.css']
+})
+export class InputList1Component
+ {
+  
+  inputItem: string = '';
+  id:number=0;
+
+  todoListObj: any[] = [
+    { id: 1, itemName: "learn react js basic", isDone: true },
+    { id: 2, itemName: "prectice react js", isDone: false },
+    { id: 3, itemName: "learn redux", isDone: false },
+    { id: 4, itemName: "code portfolio in react", isDone: false },
+    { id: 5, itemName: "learn react native", isDone: false }
+  ];
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+
+  addNew_click() {
+    let todoObj: any = new Object();
+    todoObj.itemName = this.inputItem;
+    this.todoListObj.push(todoObj);
+  }
+
+
+  delete_click(id: number): void {
+    let index = this.todoListObj.findIndex(item => item.id == id);
+    this.todoListObj.splice(index, 1);
+  }
+
+
+
+
+  select_click(id: number) {
+    let obj: any = this.todoListObj.filter(item => item.id == id);
+    this.inputItem = obj[0].itemName;
+    // obj.itemName = this.inputItem; 
+    this.id = id;
+
+  }
+
+
+  update_click() 
+  {
+    let obj: any = this.todoListObj.filter(item => item.id == this.id);
+   
+    obj[0].itemName = this.inputItem; 
+    obj[0].id = this.id; 
+   
+  }
+
+  
+
+
+  DeleteDone_click() {
+    this.todoListObj = this.todoListObj.filter(todo => todo.isDone == false);
+
+  }
+
+  DeleteAll_click() {
+    this.todoListObj = [{}];
+  }
+
+
+
+}
